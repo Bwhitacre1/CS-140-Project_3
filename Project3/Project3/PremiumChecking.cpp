32c38ff57adf552c777ca95c0eb82f2fe05e04b4
Oz-Guzman <75756076+Oz-Guzman@users.noreply.github.com>
2020-12-09 15:40:42 -0600

    Added withdraw() method

--- Project3/Project3/PremiumChecking.cpp
+++ Project3/Project3/PremiumChecking.cpp
@@ -12,7 +12,17 @@ PremiumChecking::PremiumChecking(std::string firstName, std::string lastName) {
 }
 
 void withdraw(unsigned long long amount) {
-
+if (Account::balance <= amount) {
+		if (Account::balance - amount <= -200) {
+			std::cout << "Unable to withdraw due to low funds" << std::endl;
+		}
+		else {
+			Account::balance -= amount;
+		}
+	}
+	else {
+		Account::balance -= amount;
+	}
 }
 
 void PremiumChecking::setMonthlyFee(unsigned long long fee) {
