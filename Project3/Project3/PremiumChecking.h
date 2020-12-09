bbf7d5443d8eca3f7d491e1ebd4eb14b28f83118
Oz-Guzman <75756076+Oz-Guzman@users.noreply.github.com>
2020-12-09 15:41:25 -0600

    Made inheritance public

--- Project3/Project3/PremiumChecking.h
+++ Project3/Project3/PremiumChecking.h
@@ -2,16 +2,16 @@
 #include "Account.h"
 
 
-class PremiumChecking : protected Account {
+class PremiumChecking : public Account {
 public:
 	PremiumChecking();
 	PremiumChecking(std::string firstName, std::string lastName);
 	void withdraw(unsigned long long amount);
 	void chargeMonthlyFee();
-	unsigned long long getMonthlyFee() ;
+	unsigned long long getMonthlyFee();
 	void setMonthlyFee(unsigned long long fee);
 	virtual void display() const override;
-	
+
 private:
 	unsigned long long monthlyFee = 5.75;
 };
